ec62f1cff41bd9af88d22bae3b9627f4f8127cf2
Showmik Haque <showmik.haque@gmail.com>
2023-06-17 02:43:23 +0600

    .

--- TextCleaner/View/MainWindow.xaml.cs
+++ TextCleaner/View/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using Xceed.Wpf.Toolkit;
 
@@ -15,26 +14,16 @@ namespace TextCleaner.View
         {
         }
 
-        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
-        {
-            var thumb = (Thumb)sender;
-            var richTextBox = (Xceed.Wpf.Toolkit.RichTextBox)thumb.Parent;
-
-            richTextBox.Width = Math.Max(richTextBox.ActualWidth + e.HorizontalChange, thumb.DesiredSize.Width);
-            richTextBox.Height = Math.Max(richTextBox.ActualHeight + e.VerticalChange, thumb.DesiredSize.Height);
-        }
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             double windowHeight = e.NewSize.Height;
-            double maxRowHeight = windowHeight - 380; // Adjust the division value as needed
+            double maxRowHeight = windowHeight - 380;
 
-            // Update the MaxHeight property of the row containing the GridSplitter
-            if(maxRowHeight > 0)
+            if (maxRowHeight > 0)
             {
-                grid.RowDefinitions[0].MaxHeight = maxRowHeight;
+                MainGrid.RowDefinitions[0].MaxHeight = maxRowHeight;
             }
-            
         }
     }
 }
