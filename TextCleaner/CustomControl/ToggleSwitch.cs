2dfd729e8c12353701107533fb1a811f923f0031
Showmik Haque <showmik.haque@gmail.com>
2023-06-16 17:32:52 +0600

    Toggle Swich Implemented

---
+++ TextCleaner/CustomControl/ToggleSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextCleaner.CustomControl
+{
+    public class ToggleSwitch : ToggleButton
+    {
+        static ToggleSwitch()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleSwitch), new FrameworkPropertyMetadata(typeof(ToggleSwitch)));
+        }
+    }
+}
