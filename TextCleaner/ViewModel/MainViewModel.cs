6d6e5efb291458c6460debcf01af122c4c9d9f7a
Showmik Haque <showmik.haque@gmail.com>
2023-06-17 04:34:02 +0600

    Word and Character counter implemented and removed animation from toggle switch

--- TextCleaner/ViewModel/MainViewModel.cs
+++ TextCleaner/ViewModel/MainViewModel.cs
@@ -1,13 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace TextCleaner.ViewModel
 {
-    partial class MainViewModel : ObservableObject
+    internal partial class MainViewModel : ObservableObject
     {
         [ObservableProperty] private bool _trim;
         [ObservableProperty] private bool _removeLeadSpace;
@@ -17,6 +15,42 @@ namespace TextCleaner.ViewModel
         [ObservableProperty] private bool _removeLineBreaks;
         [ObservableProperty] private bool _wrapLines;
 
-        [ObservableProperty] private string _mainText;
+        private string _mainText;
+
+        private int _wordCount;
+        private int _characterCount;
+
+        public int WordCount { get => _wordCount; set => SetProperty(ref _wordCount, value); }
+        public int CharacterCount { get => _characterCount; set => SetProperty(ref _characterCount, value); }
+       
+
+        public string MainText
+        {
+            get => _mainText;
+            set
+            {
+                SetProperty(ref _mainText, value);
+                WordCount = CountWords(_mainText);
+                CharacterCount = CountCharacters(_mainText);
+            }
+        }
+
+        public MainViewModel()
+        {
+            WrapLines = true;
+            
+        }
+
+        private int CountWords(string text)
+        {
+            string[] words = text.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+
+        private int CountCharacters(string text)
+        {
+            string cleanedText = Regex.Replace(text, @"\s", "");
+            return cleanedText.Length;
+        }
     }
 }
