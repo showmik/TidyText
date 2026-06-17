6240b8529f3c578956e7dc1797db677fe80c814b
Intisar Bin Naim <showmik.haque@gmail.com>
2026-06-17 11:42:01 +0600

    feat(helpers): add PasswordBoxHelper for MVVM binding
    Add a helper class that enables two-way data binding for WPF PasswordBox
    controls, which do not natively support binding. The helper uses attached
    properties to synchronize the Password property with a view model property
    while preventing infinite update loops.

---
+++ src/TidyText.App/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,56 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TidyText.App.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordBoxHelper), new PropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static readonly DependencyProperty BindPasswordProperty =
+            DependencyProperty.RegisterAttached("BindPassword", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false, OnBindPasswordChanged));
+
+        private static bool _isUpdating;
+
+        public static void SetBindPassword(DependencyObject dp, bool value) => dp.SetValue(BindPasswordProperty, value);
+        public static bool GetBindPassword(DependencyObject dp) => (bool)dp.GetValue(BindPasswordProperty);
+        public static string GetBoundPassword(DependencyObject dp) => (string)dp.GetValue(BoundPasswordProperty);
+        public static void SetBoundPassword(DependencyObject dp, string value) => dp.SetValue(BoundPasswordProperty, value);
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox box)
+            {
+                box.PasswordChanged -= HandlePasswordChanged;
+                if (!_isUpdating)
+                {
+                    box.Password = (string)e.NewValue ?? string.Empty;
+                }
+                box.PasswordChanged += HandlePasswordChanged;
+            }
+        }
+
+        private static void OnBindPasswordChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            if (dp is PasswordBox box)
+            {
+                var wasBound = (bool)(e.OldValue);
+                var needToBind = (bool)(e.NewValue);
+
+                if (wasBound) box.PasswordChanged -= HandlePasswordChanged;
+                if (needToBind) box.PasswordChanged += HandlePasswordChanged;
+            }
+        }
+
+        private static void HandlePasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox box)
+            {
+                _isUpdating = true;
+                SetBoundPassword(box, box.Password);
+                _isUpdating = false;
+            }
+        }
+    }
+}
